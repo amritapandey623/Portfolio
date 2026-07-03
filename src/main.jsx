9e2d1b3b8665b585d716bc4b07a99f3219d933fe
amritapandey623 <amritapandey3210@gmail.com>
2026-07-03 16:35:40 +0530

    Resume updated

--- src/main.jsx
+++ src/main.jsx
@@ -160,7 +160,7 @@ function Navbar() {
           <a className="nav-pill tooltip" href="https://www.linkedin.com/in/amrita-pandey-3s" target="_blank" rel="noreferrer" data-tip="Open LinkedIn profile">
             LinkedIn
           </a>
-          <a className="resume tooltip" href="https://drive.google.com/file/d/1RT4LlPaSLaq1ozZRZ_s2SL1mLQwEZfHi/view?usp=sharing" target="_blank" rel="noreferrer" data-tip="Download resume">
+          <a className="resume tooltip" href="https://drive.google.com/file/d/1oTKXpURk7TtGtn9NyADUS81PoR9lP5oB/view?usp=sharing" target="_blank" rel="noreferrer" data-tip="Download resume">
             <Download size={16} />
             Resume
           </a>
